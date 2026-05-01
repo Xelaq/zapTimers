50c2396010828aa87cba5193c84f3414b97db7a2
what is this? <70353572+Xelaq@users.noreply.github.com>
2026-05-02 01:02:37 +0300

    Update sh_zap_timers.lua

--- lua/autorun/sh_zap_timers.lua
+++ lua/autorun/sh_zap_timers.lua
@@ -1,24 +1,17 @@
-local function xpcall_callback(err)
-	return debug.traceback(tostring(err),2)
-end
-
 zapTimer = zapTimer or {}
 
+local hashTable = {}
 local timers = {}
 
-local function zapTimerStart(id)
-	local t = timers[id]
-	if not t then
-		error("Tried to start nonexistant timer: "..tostring(id),2)
-	end
-	t.on = true
-	t.timeDiff = nil
-	t.lastExec = CurTime()
-	return true
+function zapTimer.Timers()
+	return timers
 end
 
+function zapTimer.Create(hash, delay, reps, func)
+	if not hash then
+		error("Invalid timer name: "..tostring(hash),2)
+	end
 
-function zapTimer.Create(delay, reps, func)
 	if type(delay) ~= "number" or delay < 0 then
 		error("Invalid timer delay: "..tostring(delay),2)
 	end
@@ -31,9 +24,36 @@ function zapTimer.Create(delay, reps, func)
 
 	local id = #timers + 1
 
+	hash = tostring(hash)
+
+	hashTable[hash] = id
+
 	timers[id] = {
+		hash = hash,
 		delay = delay,
 		reps = reps == 0 and -1 or reps,
+		func = func,
+		on = false,
+		lastExec = 0,
+	}
+	
+	zapTimer.Start(id)
+end
+
+function zapTimer.Simple(delay, func)
+	if type(delay) ~= "number" or delay < 0 then
+		error("Invalid timer delay: "..tostring(delay),2)
+	end
+	if type(func) ~= "function" and not (debug.getmetatable(func) and debug.getmetatable(func).__call) then
+		error("Invalid timer function: "..tostring(func),2)
+	end
+
+	local id = #timers + 1
+
+	timers[id] = {
+		id = id,
+		delay = delay,
+		reps = 1,
 		func = func,
 		on = false,
 		lastExec = 0,
@@ -45,41 +65,55 @@ function zapTimer.Create(delay, reps, func)
 end
 
 
-function zapTimer.Start(id)
-	local t = timers[id]
+function zapTimer.Start(hash)
+	local t = hashTable[hash]
 	if not t then
-		error("Tried to start nonexistant timer: "..tostring(id),2)
+		error("Tried to start nonexistant timer: "..tostring(hash),2)
 	end
+
+	t = timers[t]
+
 	t.on = true
 	t.timeDiff = nil
 	t.lastExec = CurTime()
 	return true
 end
 
-function zapTimer.Stop(id)
-	local t = timers[id]
+function zapTimer.Stop(hash)
+	local t = hashTable[hash]
 	if not t then return false end
+
+	t = timers[t]
+
 	t.on = false
 	t.timeDiff = nil
 	return true
 end
 
-function zapTimer.Pause(id)
-	local t = timers[id]
+function zapTimer.Pause(hash)
+	local t = hashTable[hash]
 	if not t then return false end
+
+	t = timers[t]
+
 	t.on = false
 	t.timeDiff = CurTime() - t.lastExec
 	return true
 end
 
-function zapTimer.UnPause(id)
-	local t = timers[id]
-	if not t or not t.timeDiff then
+function zapTimer.UnPause(hash)
+	local t = hashTable[hash]
+	if not t then
 		error("Tried to unpause nonexistant timer: "..tostring(id),2)
 	end
+
+	t = timers[t]
+
 	if not t.timeDiff then
 		error("Tried to unpause nonpaused timer: "..tostring(id),2)
 	end
+
+
 	
 	t.on = true
 	t.lastExec = CurTime() - t.timeDiff
@@ -87,9 +121,9 @@ function zapTimer.UnPause(id)
 	return true
 end
 
-function zapTimer.Adjust(id, delay, reps, func)
-	local t = timers[id]
-	if not t or not t.timeDiff then
+function zapTimer.Adjust(hash, delay, reps, func)
+	local t = hashTable[hash]
+	if not t then
 		error("Tried to adjust nonexistant timer: "..tostring(id),2)
 	end
 	if type(delay) ~= "number" or delay < 0 then
@@ -111,68 +145,67 @@ function zapTimer.Adjust(id, delay, reps, func)
 	return true
 end
 
-function zapTimer.Destroy(id)
-	timers[id] = nil
+local function fastTimerRemove(index)
+	local tbl = timers
+    local c = #tbl
+
+    local tmr = tbl[index]
+
+	if index >= c or c == 1 then
+		tbl[index] = nil
+    else
+    	local hash = tbl[c].hash
+
+        tbl[index] = tbl[c]
+        tbl[c] = nil
+
+        hashTable[hash] = index
+    end
+
+    if tmr.hash then hashTable[tmr.hash] = nil end
+end
+
+function zapTimer.Destroy(hash)
+
+	if type(hash) ~= "string" then error("Timer name must be string!") end
+
+	local id = hashTable[hash]
+
+	if not id then error("Invalid timer "..tostring(delay),2) end
+
+	fastTimerRemove(id)
 end
 zapTimer.Remove = zapTimer.Destroy
 
-function zapTimer.Simple(delay, func)
-	if type(delay) ~= "number" or delay < 0 then
-		error("Invalid timer delay: "..tostring(delay),2)
-	end
-	if type(func) ~= "function" and not (debug.getmetatable(func) and debug.getmetatable(func).__call) then
-		error("Invalid timer function: "..tostring(func),2)
-	end
+local cache_pcall = pcall
 
-	local id = #timers + 1
-
-	timers[id] = {
-		delay = delay,
-		reps = 1,
-		func = func,
-		on = false,
-		lastExec = 0,
-	}
-	
-	zapTimer.Start(id)
-
-	return id
-end
-
-local tblCount = table.Count
-
-function zapTimer.Timers()
-	return timers
-end
-
-function zapTimer.Check()
+local function zapTimerCheck()
 	local t = CurTime()
-
-	--#	5.0430000374035e-06
-	--table.Count	7.3619999966468e-06
-	--table.Count (cached)	6.6449999940232e-06
 
 	for i=1, #timers do
 		local tmr = timers[i]
 
-		if not tmr then continue end
+		if tmr == nil then 
+			continue end
 
-		if tmr.lastExec + tmr.delay <= t then
-			local ok, err = xpcall(tmr.func, xpcall_callback)
+		local timerID = i
+
+		if tmr.lastExec + tmr.delay <= t and tmr.on then
+			local ok, err = cache_pcall(tmr.func)
+
 			if not ok then
 				ErrorNoHalt(err)
 
-				timers[i] = nil
+				fastTimerRemove(timerID)
 			else
-				tmr.lastExec = t
+				tmr.lastExec = t + tmr.delay
 				tmr.reps = tmr.reps - 1
 				if tmr.reps == 0 then
-
-					timers[i] = nil
+					fastTimerRemove(timerID)
 				end
 			end
 		end
 	end
 end
 
-hook.Add("Think", "CheckZapTimers", zapTimer.Check)
+hook.Add("Think", "CheckZapTimers", zapTimerCheck)
